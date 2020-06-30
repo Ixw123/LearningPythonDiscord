5f56e57b514d44b54bdf244f36f66e1f40bde853
Ixw123 <micah.church.a@gmail.com>
2020-06-29 23:05:17 -0400

    Initial test script

---
+++ TestScripts/TestInstall.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rd
+import math as m
+
+def main():
+    print("This is just a test file")
+    x = np.linspace(0, 2*m.pi, 200)
+    y = np.sin(x)
+
+    plt.plot(x, y)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
